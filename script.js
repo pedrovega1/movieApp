2f21d8aafd60af843625352788dda2f5489f9472
pedrovega1 <luidzhi.vegas@mail.ru>
2022-12-30 14:00:09 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+const API_KEY = 'api_key=81af8889cda6cf8e9ddd5d9e82b125bc';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+
+const main = document.getElementById ('main');
+
+getMovies(API_URL);
+
+
+function getMovies(url){
+    fetch(url).then(res => res.json()).then(data => {
+        
+        console.log(data.results)
+        
+})
+}
+
+function showMovies(data){
+    main.innerHTML = '';
+    data.forEach(movie => {
+        const{title,vote_average,poster_path,overview} = movie;
+        const movieEl = document.createElement('div')
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `
+        
+        <img src="${IMG_URL+poster_path}" alt="${title}">
+        <div class="movie-info">
+            <h3>${title}</h3>
+            <span class="${getColor(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+        <h3>Overview</h3>
+           ${overview}
+        </div> 
+        
+        `
+        main.appendChild(movieEl);
+    })
+    
+    
+}
+
+function getColor(vote){
+    if (vote >= 8){
+        return 'green'
+    }
+    else if(vote >=5){
+        return "orange"
+    }
+    else{
+        return "red"
+    }
+
+}
